843bcac66f03dc2ce2fd11e1bf79abe98c2ce914
Fabrizio W <fabrizio2210@gamil.com>
2020-11-08 18:39:33 +0100

    Add new_project form

--- src/vue/src/views/HomePage.vue
+++ src/vue/src/views/HomePage.vue
@@ -1,29 +1,53 @@
 <template>
     <div>
-        <h1>Hi {{user.firstName}}!</h1>
-        <p>You're logged in with Vue + Vuex & JWT!!</p>
-        <h3>Users from secure api end point:</h3>
+        <form @submit.prevent="handleSubmit">
+            <div class="form-group">
+                <label for="projectname">Project Name</label>
+                <input type="text" v-model="projectname" name="projectname" class="form-control" :class="{ 'is-invalid': submitted && !projectname }" />
+                <div v-show="submitted && !projectname" class="invalid-feedback">Project Name is required</div>
+            </div>
+            <div class="form-group">
+                <button class="btn btn-primary" :disabled="creating" >New Project</button>
+            </div>
+        </form>
+        <h3>Projects from secure api end point:</h3>
         <em v-if="projects.loading">Loading projects...</em>
         <span v-if="projects.error" class="text-danger">ERROR: {{projects.error}}</span>
         <ul v-if="projects.projects">
             <li v-for="project in projects.projects" :key="project.id">
-                {{project.id + ' ' + project.name}}
+                {{project.id + '. ' + project.name}}
             </li>
         </ul>
-        <p>
-            <router-link to="/login">Logout</router-link>
-        </p>
     </div>
 </template>
 
 <script>
 export default {
+    data () {
+        return {
+            projectname: '',
+            submitted: false
+        }
+    },
     computed: {
         user () {
             return this.$store.state.authentication.user;
         },
+        creating () {
+            return this.$store.state.projects.status.creating;
+        },
         projects () {
             return this.$store.state.projects.all;
+        }
+    },
+    methods: {
+        handleSubmit () {
+            this.submitted = true;
+            const { projectname } = this;
+            const { dispatch } = this.$store;
+            if (projectname) {
+                dispatch('projects/creating', { projectname });
+            }
         }
     },
     created () {
