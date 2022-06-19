91eccd29016aa8ea44e2109283b49717530b94fc
Fabrizio W <fabrizio2210@gamil.com>
2022-06-19 16:25:11 +0100

    Change how to launch dev stack due to dev upgrade

--- docker/lib/createLocalDevStack.sh
+++ docker/lib/createLocalDevStack.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -eu
 
+function docker-compose {
+  docker compose $@
+}
+
 # Supposing to deploy on x86_64 architecture
 docker build -t fabrizio2210/light_cicd-backend-dev -f docker/x86_64/Dockerfile-backend-dev .
 docker build -t fabrizio2210/light_cicd-frontend-dev -f docker/x86_64/Dockerfile-frontend-dev .
