7d7856a07e360eb8d4f15f016ae78185fe766aee
Fabrizio W <fabrizio2210@gamil.com>
2022-09-18 18:29:39 +0100

    Converting timestamps to Locale string

--- src/vue/src/views/ProjectExecutions.vue
+++ src/vue/src/views/ProjectExecutions.vue
@@ -9,7 +9,10 @@
         <span v-if="executions.error" class="text-danger">ERROR: {{executions.error}}</span>
         <ul v-if="executions.executions">
             <li v-for="execution in ordered_executions" :key="execution.id">
-                {{execution.id + '. Started:' + execution.start_time + ' Status:' + execution.rc}}
+                {{execution.id + 
+                  '. Started:' + convertDate(execution.start_time) + 
+                  ' Duration:' + elapsedTimeString(execution) + 
+                  ' Status:' + execution.rc}}
                 <router-link :to="'/projects/' + project_id + '/executions/' + execution.id + '/output'">output</router-link>
             </li>
         </ul>
@@ -52,6 +55,24 @@ export default {
             const { dispatch } = this.$store;
             const project_id = this.$route.params.project_id;
             dispatch('executions/executing', { project_id });
+        },
+        convertDate (timestamp) {
+          var myDate = new Date(timestamp * 1000);
+          return myDate.toLocaleString();
+        },
+        elapsedTimeString (execution) {
+          if (!execution.stop_time) {
+            return "Not finished yet"
+          }
+	  var sec_num = parseInt(execution.stop_time - execution.start_time, 10); 
+	  var hours   = Math.floor(sec_num / 3600);
+	  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+	  var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+	  if (hours   < 10) {hours   = "0"+hours;}
+	  if (minutes < 10) {minutes = "0"+minutes;}
+	  if (seconds < 10) {seconds = "0"+seconds;}
+	  return hours+':'+minutes+':'+seconds;
         }
     },
     created () {
