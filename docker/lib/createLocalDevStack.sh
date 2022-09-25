b8251bd8061de7196882e28152ed734bd72d834b
Fabrizio W <fabrizio2210@gamil.com>
2022-09-25 11:07:22 +0100

    Correct the stack name

--- docker/lib/createLocalDevStack.sh
+++ docker/lib/createLocalDevStack.sh
@@ -7,4 +7,4 @@ function docker-compose {
 # Supposing to deploy on x86_64 architecture
 docker build -t fabrizio2210/light_cicd-backend-dev -f docker/x86_64/Dockerfile-backend-dev .
 docker build -t fabrizio2210/light_cicd-frontend-dev -f docker/x86_64/Dockerfile-frontend-dev .
-docker-compose --project-name "ligth_cicd_dev" -f docker/lib/stack-dev.yml  up
+docker-compose --project-name "light_cicd_dev" -f docker/lib/stack-dev.yml  up
