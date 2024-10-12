ea7763b3205b85c3c0243020e6c3a7e7140a8346
Fabrizio W <fabrizio2210@gamil.com>
2024-10-12 18:08:11 +0100

    Added skeleton for executor in Golang

--- docker/lib/createLocalDevStack.sh
+++ docker/lib/createLocalDevStack.sh
@@ -7,4 +7,5 @@ function docker-compose {
 # Supposing to deploy on x86_64 architecture
 docker build -t fabrizio2210/light_cicd-backend-dev -f docker/x86_64/Dockerfile-backend-dev .
 docker build -t fabrizio2210/light_cicd-frontend-dev -f docker/x86_64/Dockerfile-frontend-dev .
+docker build -t fabrizio2210/light_cicd-executor-dev -f docker/x86_64/Dockerfile-executor-dev .
 docker-compose --project-name "light_cicd_dev" -f docker/lib/stack-dev.yml  up
---
+++ src/go/internal/rediswrapper/wrapper.go
@@ -0,0 +1,33 @@
+package rediswrapper
+
+import (
+	"context"
+	"log"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var RedisClient *redis.Client
+
+func ConnectRedis(address string) *redis.Client {
+	log.Printf("Connecting to \"%s\" for Redis", address)
+	return redis.NewClient(&redis.Options{
+		Addr: address,
+	})
+}
+
+func Publish(ctx context.Context, topic string, json []byte) error {
+	if err := RedisClient.Publish(ctx, topic, json).Err(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func WaitFor(ctx context.Context, queue string) ([]string, error) {
+	msg, err := RedisClient.BLPop(ctx, 0, queue).Result()
+	if err != nil {
+		 return nil, err
+	}
+	return msg, nil
+}
+
