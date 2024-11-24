3900724994dd771d9dfb877fdbfdb39bcf848993
Fabrizio W <fabrizio2210@gamil.com>
2024-11-24 11:43:29 +0000

    import jwt_required

--- src/resources/userlogin.py
+++ src/resources/userlogin.py
@@ -2,6 +2,7 @@ import hmac
 from models.user import UserModel
 from flask_restful import Resource, reqparse
 from flask_jwt_extended import (
+    jwt_required,
     create_access_token,
     create_refresh_token,
     get_jwt_identity
