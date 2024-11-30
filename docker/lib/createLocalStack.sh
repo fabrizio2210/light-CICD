bcc4a2d48c59d38b06af178a4c3bbd376981e80a
Fabrizio W <fabrizio2210@gamil.com>
2024-11-30 18:35:53 +0000

    Local stack update

--- docker/lib/createLocalStack.sh
+++ docker/lib/createLocalStack.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+function docker-compose {
+  docker compose $@
+}
+
 # Supposing to deploy on x86_64 architecture
-docker build -t fabrizio2210/light_cicd-backend -f docker/x86_64/Dockerfile-backend .
+docker build -t fabrizio2210/light_cicd-backend -f docker/x86_64/Dockerfile-backend --build-arg DOCKERARCH=x86_64 .
 docker build -t fabrizio2210/light_cicd-frontend -f docker/x86_64/Dockerfile-frontend .
+docker build -t fabrizio2210/light_cicd-executor -f docker/x86_64/Dockerfile-executor .
 docker-compose -f docker/lib/stack.yml  up
